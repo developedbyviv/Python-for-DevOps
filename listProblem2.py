ea4dedb33b94a196e4e056a715698f1e452ea20c
Ayushrane45 <141156845+Ayushrane45@users.noreply.github.com>
2026-01-03 23:18:26 +0530

    added list problem content

---
+++ listProblem2.py
@@ -0,0 +1,15 @@
+# # This proble is for the list in devOps
+
+# klfksdnfk
+# safnksabnfk
+# sanfkasnlfk
+# sanfkanslkf
+
+# ihwihw\
+
+# orwowou
+#  m,c
+# safnksabnfk2
+# # sanfkasnlfk2
+# # safnksabnfk2
+# # # sanfkasnlfk2
